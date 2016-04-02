5ce6765231fcf945e6f2d13dc43cf8630aa4923c
Wissam Charafeddine <wissam.charafeddine@gmail.com>
2016-04-02 11:09:57 -0400

    FORMADDED

--- app.js
+++ app.js
@@ -1,27 +1,14 @@
-// var listitems = document.getElementsByTagName('li');
-// var listitems = document.getElementsByClassName('food');
-// var favitems = document.getElementById('favorite');
-//
-// console.log(listitems);
-//
-// console.log(favitems);
-//
-// var newElement = document.createElement ('li');
-// newElement.innerHTML = 'banana cream pie';
-// var foodlist = document.getElementById('foodlist');
-// foodlist.appendChild(newElement);
-// foodlist.setAttribute('class','fancy');
-// // document.getElementById('foodlist').appendChild(newElement);
-
 // Shopping List Excersize - LAB5
 var total = 0
 var pricelist = {
   name: ['milk','cheese','eggs','bread','sugar','salt',"banana","strawberry"],
   price: [5.30,3.2,4.3,2.5,3.6,10,4.85,1.99]
 };
+
 var shoplist = ["cheese","eggs","sugar"];
+
 shoplist.forEach(function(x) {
-  var displayshoplist = document.createElement ('li');
+  var displayshoplist = document.createElement ('ol');
   var locationindex= pricelist.name.indexOf(x);
   displayshoplist.innerHTML =  x +"...$"+pricelist.price[locationindex].toFixed(2);
   div1.appendChild(displayshoplist);
@@ -32,3 +19,17 @@ var thetotal = document.createElement('h3');
 thetotal.innerHTML =  "TOTAL= $"+total.toFixed(2);
 div1.appendChild(thetotal);
 div1.setAttribute('class','fancy');
+
+function additemfun (){
+  var valueofnewitem = document.getElementById('newitem').value;
+  var valueofnewitemprice = document.getElementById('newitemprice').value;
+  var valueofnewitempriceNum = Number(valueofnewitemprice);
+  if (pricelist.name.indexOf(valueofnewitem)>-1) {
+    shoplist.push(valueofnewitem);
+  }
+  else {
+    pricelist.name.push(valueofnewitem);
+    pricelist.price.push(valueofnewitempriceNum);
+  }
+
+};
